945b114367481f61457cd4d243a932c468496c2a
Ankush7D <ankush0dutta7@gmail.com>
2026-03-08 00:31:17 +0530

    Delete loan_prediction directory

--- loan_prediction/app/model_loader.py
+++
@@ -1,4 +0,0 @@
-import joblib
-
-scaler = joblib.load("model/scaler.pkl")
-model = joblib.load("model/svc_model.pkl")
--- loan_prediction/app/schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class LoanInput(BaseModel):
-    income: float
-    age: float
-    loan_amount: float
-    credit_score: float
-    dependents: float
